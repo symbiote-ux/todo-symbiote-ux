48cffd1b0b8dfeb0408dd293bbcf9bb3f006ee94
Saurabh <58027263+symbiote-ux@users.noreply.github.com>
2020-02-02 09:53:34 +0530

    server added with tests

---
+++ server.js
@@ -0,0 +1,9 @@
+const {Server} = require('http');
+const {app} = require('./lib/handlers');
+
+const main = () => {
+  const port = 4000;
+  const server = new Server(app.serve.bind(app));
+  server.listen(port);
+};
+main();
---
+++ test/testhandlers.js
@@ -0,0 +1,30 @@
+const request = require('supertest');
+const {app} = require('../lib/handlers');
+
+describe('Get home page ', () => {
+  it('serves home page on "/" path', done => {
+    request(app.serve.bind(app))
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', 'text/html', done)
+      .expect(/ToDo/);
+  });
+});
+
+describe('Get non Existing url', () => {
+  it('responds page not found for non existing url', done => {
+    request(app.serve.bind(app))
+      .get('/badUrl')
+      .expect(404, done)
+      .expect(/Page Not Found/);
+  });
+});
+
+describe('PUT /url', () => {
+  it('responds as method not allowed', done => {
+    request(app.serve.bind(app))
+      .put('/url')
+      .expect(400, done)
+      .expect(/Method Not Allowed/);
+  });
+});
